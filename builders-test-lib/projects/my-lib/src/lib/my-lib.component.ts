ec201afcd94ba6b2f65895ecea33b8d8a6cd51c3
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2021-10-27 14:46:34 -0400

    Update `builders-test-lib`

--- builders-test-lib/projects/my-lib/src/lib/my-lib.component.ts
+++ builders-test-lib/projects/my-lib/src/lib/my-lib.component.ts
@@ -2,19 +2,11 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'lib-my-lib',
-  template: `
-    <p>
-      my-lib works!
-    </p>
-  `,
-  styles: [
-  ]
+  template: ` <p>my-lib works!</p> `,
+  styles: [],
 })
 export class MyLibComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
